a5708c0164449844f08863449024189043040e61
jdcera4 <jdcera4@gmail.com>
2023-03-02 14:32:21 -0500

    create frontend init

---
+++ Frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './componets/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gestionar Empleados</h1>
+      <Form></Form>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/componets/Form.jsx
@@ -0,0 +1,27 @@
+function Form() {
+  return (
+    <form>
+        <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">Cedula</label>
+            <input type="number" className="form-control" id="exampleInputCedula"  />
+        </div>
+        <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Nombre</label>
+            <input type="text" className="form-control" id="exampleInputNombre"/>
+        </div>
+
+        <div className="mb-3">
+            <label for="disabledSelect" className="form-label">Cargo</label>
+            <select id="disabledSelect" className="form-select">
+                <option>Mercadeo</option>
+                <option>Ti</option>
+            </select>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+
+    </form>
+  );
+}
+
+export default Form;

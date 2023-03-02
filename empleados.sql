332f04dabeb4794ff52aaf52f78cdca099f6df5f
jdcera4 <jdcera4@gmail.com>
2023-03-02 08:15:43 -0500

    Script create workers

---
+++ empleados.sql
@@ -0,0 +1,9 @@
+INSERT INTO empleados(cedula, name, role) values
+(1009836, "David Cera", 2),
+(1009889, "Diego Ramirez", 1),
+(1009806, "Veronica Alzate", 2),
+(1009876, "Alejandro Rendon", 3),
+(1009856, "Mauricio Restrepo", 2),
+(1009823, "Laura Bedoya", 2),
+(1009076, "Daniela Alvares", 5),
+(1009126, "Camilo Monsalve", 2)
